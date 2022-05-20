10608b9e0b3d5cef16dc1a4302bcd5bbb8e2d610
Vikram Negi <viknegi0@gmail.com>
2022-05-21 00:12:01 +0530

    if/else Music/ dir exists

--- yt-audio.sh
+++ yt-audio.sh
@@ -6,17 +6,19 @@
 # sudo ln -s /usr/bin/python3 /usr/local/bin/python
 
 yt_url="$1"
-music_dir="~/Music/"
+music_dir="$HOME/Music/"
 
 # cd ~/Music/ || mkdir -p ./Music/ && cd Music/
 
 if [ -d "$music_dir" ]; then
-  echo "${music_dir} exists"
+  # echo "${music_dir} exists!"
+  cd $music_dir
 else
-  echo "${music_dir} not found"
+  echo "Notification: ${music_dir} was not found in $HOME directory."
+  mkdir $music_dir
+  cd $music_dir
 fi
 
 # youtube-dl --audio-quality 160k --restrict-filenames --extract-audio --audio-format mp3 --embed-thumbnail -o "%(title)s.%(ext)s" $yt_url
 
-
-touch hello.txt
+# touch hello.txt
