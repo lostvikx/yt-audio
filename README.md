de3e182db78971e5f548cb66a3d11d7b4410e032
Vikram Negi <viknegi0@gmail.com>
2022-05-21 00:38:08 +0530

    160kbps quality

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # YTAudio
 
-Download audio from a YouTube video
+YouTube link to mp3 file.
+
+## Install Dependencies
+
+`youtube-dl` download instructions
+
+```bash
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+
+# If you get python not found error
+sudo ln -s /usr/bin/python3 /usr/local/bin/python
+```
+
+## Usage
+
+```bash
+yt-audio "https://www.youtube.com/watch?v=Uk1hv6h7O1Y&ab_channel=Bazzi"
+```
--- yt-audio.sh
+++ yt-audio.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# youtube-dl download instructions
-# sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
-# sudo chmod a+rx /usr/local/bin/youtube-dl
-# sudo ln -s /usr/bin/python3 /usr/local/bin/python
-
 # $1 is a required argument
 if [ "$1" = "" ]
 then
@@ -18,7 +13,7 @@ music_dir="$HOME/Music/"
 # cd ~/Music/ || mkdir -p ./Music/ && cd Music/
 
 if [ -d "$music_dir" ]; then
-  # echo "${music_dir} exists!"
+  echo "Notification: ${music_dir} exists!"
   cd $music_dir
 else
   echo "Notification: ${music_dir} was not found in $HOME directory."
@@ -26,6 +21,5 @@ else
   cd $music_dir
 fi
 
-# youtube-dl --audio-quality 160k --restrict-filenames --extract-audio --audio-format mp3 --embed-thumbnail -o "%(title)s.%(ext)s" $YouTube_URL
+youtube-dl --audio-quality 160k --restrict-filenames --extract-audio --audio-format mp3 --embed-thumbnail -o "%(title)s.%(ext)s" $YouTube_URL
 
-# touch $YouTube_URL.txt
